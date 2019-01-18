69aa2c46c83b257a4526fd8edafcfa0f7da72268
Charlie Olofsson <chol0521@storsjogymnasiet.se>
2019-01-18 15:33:33 +0100

    Commit 2:scanner

--- app/src/main/java/com/example/bluffaktura/MainActivity.java
+++ app/src/main/java/com/example/bluffaktura/MainActivity.java
@@ -3,11 +3,37 @@ package com.example.bluffaktura;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+             public class ReadNSearchFile {
+
+               private static Scanner x;
+
+              public static void main(String[] args) {
+                String filepath = "tutorial.txt";
+                String searchTerm = "search";
+             }
+            public static void readRecord (String searchTerm,String filepath) {
+
+                  try
+                  {
+
+                  }
+
+                  catch (Exception e)
+                  {
+
+                  }
+
+                  
+            }
+        }
     }
 }
